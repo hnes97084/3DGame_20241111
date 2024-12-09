f726f6e5f38b11886e6068d60b076f1d0dba78a7
hnes97084 <hnes97084@gmail.com>
2024-12-09 11:34:41 +0800

    20241209

--- Assets/Scripts/InteractionSystem.cs
+++ Assets/Scripts/InteractionSystem.cs
@@ -7,7 +7,12 @@ namespace Betty
     /// </summary>
     public class InteractionSystem : MonoBehaviour
     {
-        
+        //繪製圖示事件 ODG : 僅在 Unity 中顯示
+        private void OnDrawGizmos()
+        {
+            // 1. 決定顏色
+
+        }
     }
 }
 
