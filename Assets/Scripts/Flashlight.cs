e067df3d436e8face5425e997a18e31bf171c695
hnes97084 <hnes97084@gmail.com>
2024-12-16 11:28:01 +0800

    20241216

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Betty
+{
+    /// <summary>
+    /// 手電筒
+    /// </summary>
+    public class Flashlight : MonoBehaviour, IInteraction
+    {
+        public void Interaction()
+        {
+            print($"<color=#37f>互動 : {name}</color>");
+        }
+
+        public void PickUp()
+        {
+            print($"<color=#37f>撿取 : {name}</color>");
+        }
+    }
+}
+
--- Assets/Scripts/InteractionSystem.cs
+++ Assets/Scripts/InteractionSystem.cs
@@ -7,11 +7,47 @@ namespace Betty
     /// </summary>
     public class InteractionSystem : MonoBehaviour
     {
-        //繪製圖示事件 ODG : 僅在 Unity 中顯示
+        [Header("圖示資料")]
+        [SerializeField]
+        private Color rayColor = Color.red;
+        [SerializeField, Range(1, 20)]
+        private float rayLength = 10;
+        [SerializeField]
+        private LayerMask rayLayer;
+
+        // 繪製圖示事件 ODG : 僅在 Unity 內顯示
         private void OnDrawGizmos()
         {
             // 1. 決定顏色
+            Gizmos.color = rayColor;
+            // 2. 繪製圖示
+            // 圖示的繪製射線(中心點，方向 * 距離)
+            Gizmos.DrawRay(transform.position, transform.forward * rayLength);
+        }
 
+        private void Update()
+        {
+            Raycast();
+        }
+
+        private void Raycast()
+        {
+            // 物理的射線碰撞(中心點，方向，碰到的物件，距離，圖層)
+            bool isHit = Physics.Raycast(
+                transform.position, transform.forward, out RaycastHit hit, rayLength, rayLayer);
+
+            // 如果 碰到的物件 為 空的 就 跳出
+            if (hit.collider == null) return;
+
+            //判斷是否有互動介面
+            if (hit.collider.TryGetComponent(out IInteraction interaction)) 
+            {
+                // 如果按下左鍵 就對互動物件執行撿取功能
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    interaction.PickUp();
+                }
+            }
         }
     }
 }
